96c531fa134bbea295a89af68d5abe1e874c439d
vsvijay987 <vsvijju987@gmail.com>
2022-03-27 02:48:46 +0530

    type fix link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 //@ts-ignore
-import Link from "next/Link";
+import NextLink from "next/Link";
 
 const Header = () => {
   return (
     <header className="flex justify-between p-5 max-w-7xl mx-auto">
       <div className="flex items-center space-x-8">
-        <Link href="/">
+        <NextLink  href="/">
           <h2>Anayata</h2>
-        </Link>
+        </NextLink>
         <div className="inline-flex items-center space-x-5">
           <h3 className="text-white px-4 py-1 rounded-full" style={{backgroundColor: '#a75ca5'}}>
             Home
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 //@ts-ignore
-import Link from "next/link";
+import NextLink  from "next/link";
 
 import Banner from "../components/Banner";
 import Header from "../components/Header";
@@ -21,7 +21,7 @@ const Home = ({ posts }: Props) => {
       {/* posts */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
         {posts.map((post) => (
-          <Link key={post._id} href={`/post/${post.slug.current}`}>
+          <NextLink  key={post._id} href={`/post/${post.slug.current}`}>
             <div className="border rounded-lg group cursor-pointer overflow-hidden">
               <img
                 className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
@@ -41,7 +41,7 @@ const Home = ({ posts }: Props) => {
                 />
               </div>
             </div>
-          </Link>
+          </NextLink>
         ))}
       </div>
     </div>
